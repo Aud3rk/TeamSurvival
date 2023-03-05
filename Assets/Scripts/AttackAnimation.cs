e40715e26b8c5744a76e92e398cfd8cee565c69d
Aud3rk <sadykov.zaxar@mail.ru>
2023-03-05 17:22:43 +0300

    моделька и анимация врага, локация (свет, деревья, туман и тд)

--- Assets/Scripts/AttackAnimation.cs
+++ Assets/Scripts/AttackAnimation.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class AttackAnimation : MonoBehaviour
 {
     private Animator _animation;
-    private float _timeCD = 4f;
+    
 
     private void Start()
     {
@@ -18,26 +18,7 @@ public class AttackAnimation : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.C))
             _animation.SetTrigger("Play");
-        Attack();
     }
 
-    private void Attack()
-    {
-        _timeCD -= Time.deltaTime;
-        if (inDistance())
-        {
-            if (_timeCD <= 0)
-            {
-                _timeCD = 5f;
-                var entity = Contexts.sharedInstance.game.CreateEntity();
-                entity.AddDamage(Contexts.sharedInstance.game.playerEntity.view.value.gameObject, 25);
-            }
-        }
-    }
-
-    private bool inDistance()
-    {
-        return (Vector3.Distance(transform.position,
-            Contexts.sharedInstance.game.playerEntity.view.value.transform.position) < 2f);
-    }
+    
 }
--- Assets/Scripts/System/PlayerAttackSystem.cs
+++ Assets/Scripts/System/PlayerAttackSystem.cs
@@ -17,20 +17,21 @@ public class PlayerAttackSystem : IExecuteSystem
         _timeCD -= Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.C))
         {
-            if(_timeCD<=0)
+            
+            Ray ray = new Ray(_contexts.game.playerEntity.view.value.transform.position,
+                _contexts.game.playerEntity.view.value.transform.forward);
+            /*Vector3 point = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2, 0);
+            Ray ray = Camera.main.ScreenPointToRay(point);*/
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 2))
             {
-                _timeCD = 2f;
-                Ray ray = new Ray(_contexts.game.playerEntity.view.value.transform.position,
-                    _contexts.game.playerEntity.view.value.transform.forward);
-                /*Vector3 point = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2, 0);
-                Ray ray = Camera.main.ScreenPointToRay(point);*/
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit, 2))
+                if (_timeCD <= 0)
                 {
+                    _timeCD = 1f;
                     if (hit.transform.gameObject.CompareTag(DAMAGEABLE_TAG))
                     {
                         var entity = _contexts.game.CreateEntity();
-                        entity.AddDamage(hit.transform.gameObject, 20);
+                        entity.AddDamage(hit.transform.gameObject, 25);
                     }
                 }
             }
